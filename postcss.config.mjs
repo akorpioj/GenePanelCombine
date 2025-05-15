1d438d33b752519bec3f03569bbbeb1704e79f42
Anita <anita.korpioja@gmail.com>
2025-05-15 22:20:58 +0300

    Tailwindcss. Clear All button

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
+// This configuration file is for PostCSS, which is a tool for transforming CSS with JavaScript plugins.
+// The file exports an object with a plugins property, which contains two plugins:
+// 1. "@tailwindcss/postcss": This is the Tailwind CSS plugin for PostCSS, which allows you to use Tailwind CSS in your project.
+// 2. "autoprefixer": This plugin automatically adds vendor prefixes to your CSS rules, ensuring better browser compatibility.
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./static/**/*.{js,css}",
+    "./src/**/*.{js,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
